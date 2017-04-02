1dac6352c878399536e913ace9a80bcdf9e3f958
Li Jiang <lijgame@gmail.com>
2017-04-02 13:34:30 +0800

    disable cuda on openmvs

--- build_openMVS.sh
+++ build_openMVS.sh
@@ -6,5 +6,6 @@ cd ~/codes/openMVS
 git remote add remote https://github.com/cdcseacave/openMVS.git
 mkdir -p ~/codes/openMVS_build
 cd ~/codes/openMVS_build
-cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-5 -DCMAKE_CXX_COMPILER=/usr/bin/g++-5 -DTESTS_ENABLED=OFF ~/codes/openMVS
+cmake -DWITH_OPENMP=ON -DENABLE_AVX=ON -DENABLE_AVX2=ON -DENABLE_SSE42=ON \
+-DTESTS_ENABLED=OFF -DOpenMVS_USE_CUDA=OFF ~/codes/openMVS
 make -j$num_cpus
