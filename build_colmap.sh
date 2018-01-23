1a0af7dbddd3b2f01a146086d2203b5502bfb6cb
Li Jiang <lijgame@gmail.com>
2018-01-23 12:48:34 +0800

    move build folder

--- build_colmap.sh
+++ build_colmap.sh
@@ -19,7 +19,7 @@ sudo apt-get -y install \
 git clone https://github.com/lijgame/colmap.git ~/codes/colmap
 cd ~/codes/colmap
 git remote add remote https://github.com/colmap/colmap.git
-mkdir -p ~/codes/colmap_build
-cd ~/codes/colmap_build
+mkdir -p ~/codes/colmap/build
+cd ~/codes/colmap/build
 cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-5 -DCMAKE_CXX_COMPILER=/usr/bin/g++-5 -DTESTS_ENABLED=OFF ~/codes/colmap
 make -j$num_cpus
