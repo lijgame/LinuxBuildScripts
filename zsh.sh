52da4a2d32ba5dbb8b92baa1157021f3fc0dfba5
Li Jiang <lijgame@gmail.com>
2018-07-12 19:02:50 +0800

    update zsh

--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,4 @@
-sudo apt install zsh
+sudo apt install zsh curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo bindkey "^[[1;3C" forward-word >> ~/.zshrc
 echo bindkey "^[[1;3D" backward-word >> ~/.zshrc
