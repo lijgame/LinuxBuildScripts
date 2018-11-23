184b69e3f9d98e0d8663e5f1215f0ffea96bb933
Li Jiang <lijgame@gmail.com>
2018-11-23 13:23:14 +0800

    update build opencv with ninja

--- build_opencv.sh
+++ build_opencv.sh
@@ -5,12 +5,12 @@ sudo apt install -y python-dev python-numpy python3-dev python3-numpy cmake git 
 git clone --recursive  https://github.com/lijgame/opencv.git ~/codes/opencv
 cd ~/codes/opencv
 git remote add remote https://github.com/opencv/opencv.git
-mkdir -p ~/codes/opencv_build
-cd ~/codes/opencv_build
-cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-5 -DCMAKE_CXX_COMPILER=/usr/bin/g++-5 \
+mkdir -p ~/codes/opencv/build
+cd ~/codes/opencv/build
+cmake -G"Ninja" -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
 -DOPENCV_EXTRA_MODULES_PATH=~/codes/opencv/opencv_contrib/modules \
--DWITH_OPENMP=ON -DWITH_CUDA=ON -DENABLE_AVX=ON -DENABLE_AVX2=ON -DENABLE_SSE42=ON \
--DTESTS_ENABLED=OFF -DWITH_OPENGL=ON -DBUILD_TESTS=OFF -DBUILD_opencv_sfm=OFF ~/codes/opencv
+-DWITH_OPENMP=ON -DWITH_CUDA=ON -DENABLE_AVX2=ON \
+-DTESTS_ENABLED=OFF -DWITH_OPENGL=ON -DBUILD_TESTS=OFF -DBUILD_opencv_sfm=ON -DPYTHON_DEFAULT_EXECUTABLE=$(which python3) -DBUILD_opencv_cudacodec=OFF ~/codes/opencv
 
-make -j$num_cpus
-sudo make install
+ninja
+sudo ninja install
