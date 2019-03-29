44f38182cc328c38e4ba99a52f79631c4f9f1a9f
Li Jiang <lijgame@gmail.com>
2019-03-29 16:51:30 +0800

    install docker

--- cuda/disable_nouveau.sh
+++
@@ -1,3 +0,0 @@
-bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo update-initramfs -u
