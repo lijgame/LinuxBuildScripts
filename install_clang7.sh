1bcc41baf8185771586907a1561293ebda879c1c
Li Jiang <lijgame@gmail.com>
2019-04-25 04:50:26 +0000

    install clang7

---
+++ install_clang7.sh
@@ -0,0 +1,3 @@
+sudo apt install clang-7 clang-format-7 clang-tools-7  -y
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 100
