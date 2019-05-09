43d6d2271c95bfbe4d0f042129ff044d50b3e87e
Jack Li <Jack.Li@autodesk.com>
2019-05-09 22:43:25 +0800

    dev

--- run_dev_docker.sh
+++ run_dev_docker.sh
@@ -1,1 +1,4 @@
+# run first: sudo xhost +local:root
 docker run -it --name=lijgame-dev --runtime=nvidia -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/codes:/root/codes -w /root/codes -e DISPLAY=unix:1 lijgame/dev
+#docker run -it --runtime=nvidia -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/codes:/root/codes -w /root/codes -e DISPLAY=unix:0 lijgame/dev
+
