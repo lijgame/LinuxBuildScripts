a1649b6de82222bf63c5db394444fd917644d46f
Li Jiang <lijgame@gmail.com>
2019-07-18 16:15:44 +0800

    Update run_dev_docker.sh

--- run_dev_docker.sh
+++ run_dev_docker.sh
@@ -1,4 +1,2 @@
 # run first: sudo xhost +local:root
-docker run -it --name=lijgame-dev --runtime=nvidia -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/codes:/root/codes -w /root/codes -e DISPLAY=unix:1 lijgame/dev
-#docker run -it --runtime=nvidia -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/codes:/root/codes -w /root/codes -e DISPLAY=unix:0 lijgame/dev
-
+docker run -it --name=lijgame-dev --runtime=nvidia -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v $XAUTHORITY:/.xauthority -e XAUTHORITY=/.xauthority -v ~/codes:/root/codes -w /root/codes lijgame/dev
