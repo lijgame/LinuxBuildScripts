de767d322f5dbc2015f83a0dc76f1a9339491521
Li Jiang <lijgame@gmail.com>
2020-02-23 09:47:27 +0800

    update clang install

--- install_clang7.sh
+++ install_clang7.sh
@@ -1,3 +1,5 @@
-sudo apt install clang-7 clang-format-7 clang-tools-7  -y
+sudo apt install clang-7 clang-format-7 clang-tools-7 lld-7 lldb-7  -y
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 100
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 100 \
+   --slave /usr/bin/clang++ clang++ /usr/bin/clang++-7
+sudo update-alternatives --install /usr/bin/ld.lld ld.lld /usr/bin/ld.lld-7 100
