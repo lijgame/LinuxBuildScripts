8f5515332edd7e9b36df2b3fc6b815c5bb6ef7f7
Li Jiang <lijgame@gmail.com>
2020-12-08 23:46:25 +0800

    update colmap script

--- run_colmap.sh
+++ run_colmap.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-feature_extractor --image_path ./img --database_path ./db.db
-vocab_tree_matcher --database_path ./db.db --VocabTreeMatching.vocab_tree_path ~/vocab_tree-65536.bin
-mkdir ./sparse
-mapper --database_path ./db.db --image_path ./img --export_path ./sparse
-mkdir ./dense
-image_undistorter --image_path ./img --input_path ./sparse/0 --output_path ./dense --output_type COLMAP --max_image_size 4000
-dense_stereo --workspace_path ./dense workspace_format ./COLMAP --DenseStereo.geom_consistency true --DenseStereo.max_image_size 4000
-dense_fuser --workspace_path ./dense workspace_format ./COLMAP --input_type geometric --output_path ./dense/fused.ply
-dense_mesher --input_path ./dense/fused.ply --output_path ./dense/meshed.ply --DenseMeshing.depth 13 --DenseMeshing.trim 11 --DenseMeshing.color 32 
+colmap feature_extractor --image_path ./img --database_path ./db.db
+colmap vocab_tree_matcher --database_path ./db.db --VocabTreeMatching.vocab_tree_path ~/vocab_tree_flickr100K_words32K.bin
+mkdir -p ./sparse
+colmap mapper --database_path ./db.db --image_path ./img --output_path ./sparse
+mkdir -p ./dense
+colmap image_undistorter --image_path ./img --input_path ./sparse/0 --output_path ./dense --output_type COLMAP --max_image_size 2000
+colmap patch_match_stereo  --workspace_path ./dense --workspace_format COLMAP --PatchMatchStereo.geom_consistency true --PatchMatchStereo.max_image_size 2000
+colmap dense_fuser --workspace_path ./dense --workspace_format COLMAP --input_type geometric --output_path ./dense/fused.ply
+colmap dense_mesher --input_path ./dense/fused.ply --output_path ./dense/meshed.ply --DenseMeshing.depth 13 --DenseMeshing.trim 11 --DenseMeshing.color 32 
+
